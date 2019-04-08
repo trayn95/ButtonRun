b3c4cf313d15b5332ef95907ffb71d4daa6afdab
Артём Александров <trayn952@mail.ru>
2019-04-08 23:29:50 +0300

    commint

--- ButtonRun/Form1.Designer.cs
+++ ButtonRun/Form1.Designer.cs
@@ -29,12 +29,58 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(373, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Прогуливать пары плохо";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(359, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 72);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Да понял я уже";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- ButtonRun/Form1.cs
+++ ButtonRun/Form1.cs
@@ -16,5 +16,45 @@ namespace ButtonRun
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (i < 60)
+            {
+                Random r = new Random();
+                int w = r.Next(this.ClientSize.Width - button1.Size.Width);
+                int h = r.Next(this.ClientSize.Height - button1.Size.Height);
+                this.button1.Location = new System.Drawing.Point(w, h);
+            }
+            else if (i > 60)
+            {
+
+            }
+
+
+
+        }
+        int i = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int w = this.Size.Width - label1.Size.Width;
+            int h = this.Size.Height - label1.Size.Height;
+            int x = r.Next(0, w);
+            int y = r.Next(0, h);
+            label1.Location = new Point(x, y);
+            i++;
+        }
     }
 }
